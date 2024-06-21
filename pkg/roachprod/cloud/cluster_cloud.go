59519df61b7322d46a402aae2d2a53a509c93e17
Herko Lategan <herko@cockroachlabs.com>
2024-06-21 14:24:32 +0100

    roachprod: fix provider check for grow and shrink
    Previously, the provider check could fail if the cloud provider included a
    project name. The provider specified on the VM struct itself is what should be
    used in the comparison.
    
    Release Note: None
    Epic: None

--- pkg/roachprod/cloud/cluster_cloud.go
+++ pkg/roachprod/cloud/cluster_cloud.go
@@ -349,13 +349,14 @@ func GrowCluster(l *logger.Logger, c *Cluster, numNodes int) error {
 	}
 
 	providers := c.Clouds()
-	if len(providers) != 1 || providers[0] != gce.ProviderName {
+	provider := c.VMs[0].Provider
+	if len(providers) != 1 || provider != gce.ProviderName {
 		return errors.Errorf("cannot grow cluster %s, growing a cluster is currently only supported on %s",
 			c.Name, gce.ProviderName)
 	}
 
 	// Only GCE supports expanding a cluster.
-	return vm.ForProvider(gce.ProviderName, func(p vm.Provider) error {
+	return vm.ForProvider(provider, func(p vm.Provider) error {
 		return p.Grow(l, c.VMs, c.Name, names)
 	})
 }
@@ -363,7 +364,8 @@ func GrowCluster(l *logger.Logger, c *Cluster, numNodes int) error {
 // ShrinkCluster removes tail nodes from an existing cluster.
 func ShrinkCluster(l *logger.Logger, c *Cluster, numNodes int) error {
 	providers := c.Clouds()
-	if len(providers) != 1 || providers[0] != gce.ProviderName {
+	provider := c.VMs[0].Provider
+	if len(providers) != 1 || provider != gce.ProviderName {
 		return errors.Errorf("cannot shrink cluster %s, shrinking a cluster is currently only supported on %s",
 			c.Name, gce.ProviderName)
 	}
@@ -376,7 +378,7 @@ func ShrinkCluster(l *logger.Logger, c *Cluster, numNodes int) error {
 	vmsToDelete := c.VMs[len(c.VMs)-numNodes:]
 
 	// Only GCE supports shrinking a cluster.
-	return vm.ForProvider(gce.ProviderName, func(p vm.Provider) error {
+	return vm.ForProvider(provider, func(p vm.Provider) error {
 		return p.Shrink(l, vmsToDelete, c.Name)
 	})
 }
